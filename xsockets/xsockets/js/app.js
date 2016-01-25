0a4c24c95a0c12f05ceafa553dab487fd7c2a605
Per Fahén <perfahlen@gmail.com>
2016-01-25 13:08:35 +0100

    added ol project vs

---
+++ xsockets/xsockets/js/app.js
@@ -0,0 +1,96 @@
+var socketMap = socketMap || {};
+
+var map;
+
+socketMap.init = function(){
+	map = new ol.Map({
+		target: "map",
+		layers: [
+			new ol.layer.Tile({
+				source : new ol.source.OSM()
+			}),
+			new ol.layer.Vector({
+				title: "Observations",
+				source: new ol.source.Vector()
+			})
+		],
+		view: new ol.View({
+    		center: [0, 0],
+    		zoom: 2
+  		})
+	});
+};
+
+//coordinates in format[x, y], coordinates expects an array, attributes can be any object
+socketMap.createFeature = function(coordinates, attributes){
+	var point = new ol.geom.Point(coordinates);
+	var feature = new ol.Feature({
+		geometry: point,
+		attributes: attributes
+	});
+	return feature;
+};
+
+//creates rules for styling
+var createStyle = function(feature){
+	var attributes = feature.get("attributes");
+	var val = attributes.val;
+	var fill = val === 1 ? "red" : 
+		val === 2 ? "blue" : "green";
+	
+	var style = new ol.style.Style({
+		image: new ol.style.Circle({
+			fill: new ol.style.Fill({color: fill}),
+			stroke: new ol.style.Stroke({
+				color: "white",
+				width: 1
+			}),
+			radius: 50
+		})
+	});
+	return style;
+	};
+
+
+socketMap.setObservation = function(feature){
+	var observationLayer = map.getLayers().getArray()[1];
+	var source = observationLayer.getSource();
+	var currentFeature = socketMap.findFeature(feature, source) || feature;
+
+	var style = new createStyle(currentFeature);
+	currentFeature.setStyle(style);
+	source.addFeatures([currentFeature]);
+};
+
+socketMap.findFeature = function(currentFeature, source){
+	var features = source.getFeatures();
+	if (features.length === 0) return null;
+
+	var currentAttributes = currentFeature.get("attributes");
+	var currentId = currentAttributes.id;
+	var findFeatures = features.filter(function(feature){
+		var attributes = feature.get("attributes");
+		return attributes.id === currentId;
+	});
+	var feature = findFeatures.length === 1 ? findFeatures[0] : null;
+	return feature;
+};
+
+var obs1 = new socketMap.createFeature([0, 0], {id: 1, val: 1});
+var obs2 = new socketMap.createFeature([5000000, 5000000], {id: 2, val: 2});
+
+var timerId = setTimeout(function(){
+	if (map === null) return;
+
+	socketMap.setObservation(obs1);
+	socketMap.setObservation(obs2);
+	clearInterval(timerId);
+}, 10);
+
+
+socketMap.changeColor = function(){
+	var val = Math.ceil((Math.random()*10000)%3);
+	var updateObs = new socketMap.createFeature([5000000, 5000000], {id: 2, val: val});
+	socketMap.setObservation(updateObs);
+};
+
