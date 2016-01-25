fa5c643bbc15395535eebca7bd4254321d9ff49f
Per Fahén <perfahlen@gmail.com>
2016-01-25 16:25:09 +0100

    render points

--- geoLive/WebStreamedLayer/js/app.js
+++ geoLive/WebStreamedLayer/js/app.js
@@ -3,40 +3,18 @@ var socketMap = socketMap || {};
 
 var map;
 
-socketMap.init = function(){
-	map = new ol.Map({
-		target: "map",
-		layers: [
-			new ol.layer.Tile({
-				source : new ol.source.OSM()
-			}),
-			new ol.layer.Vector({
-				title: "Observations",
-				source: new ol.source.Vector()
-			})
-		],
-		view: new ol.View({
-    		center: [0, 0],
-    		zoom: 2
-  		})
-	});
-
-	var conn = new XSockets.WebSocket("ws://localhost:4502", ["GeoCars"]);
-	var controller = conn.controller("geocars");
-	controller.on("pos",sock.carMove);
-};
-
 socketMap.carMove = function (data) {
     var json = JSON.parse(data);
     var id = json.id;
     var lat = json.geodata.Lat;
     var lon = json.geodata.Lng;
-
+    console.log(id, lat, lon);
 };
 
 //coordinates in format[x, y], coordinates expects an array, attributes can be any object
 socketMap.createFeature = function(coordinates, attributes){
-	var point = new ol.geom.Point(coordinates);
+    var point = new ol.geom.Point(coordinates);
+    point.transform("EPSG:4326", "EPSG:3857");
 	var feature = new ol.Feature({
 		geometry: point,
 		attributes: attributes
@@ -95,24 +73,32 @@ socketMap.findFeature = function(currentFeature, source){
 	return feature;
 };
 
-//var obs1 = new socketMap.createFeature([0, 0], {id: 1, val: 1});
-//var obs2 = new socketMap.createFeature([5000000, 5000000], {id: 2, val: 2});
-
-//var timerId = setTimeout(function(){
-//	if (map === null) return;
-
-//	socketMap.setObservation(obs1);
-//	socketMap.setObservation(obs2);
-//	clearInterval(timerId);
-//}, 10);
-
-
-//socketMap.changeColor = function(){
-//	var val = Math.ceil((Math.random()*10000)%3);
-//	var updateObs = new socketMap.createFeature([5000000, 5000000], {id: 2, val: val});
-//	socketMap.setObservation(updateObs);
-//};
-
 socketMap.transformFeature = function (feature) {
     feature.getGeometry().transform("EPSG:4326", "EPSG:3857");
+};
+
+socketMap.init = function () {
+    map = new ol.Map({
+        target: "map",
+        layers: [
+			new ol.layer.Tile({
+			    source: new ol.source.OSM()
+			}),
+			new ol.layer.Vector({
+			    title: "Observations",
+			    source: new ol.source.Vector()
+			})
+        ],
+        view: new ol.View({
+            center: [0, 0],
+            zoom: 2
+        })
+    });
+
+    var conn = new XSockets.WebSocket("ws://localhost:4502", ["GeoCars"]);
+    var controller = conn.controller("geocars");
+    controller.on("pos", function (data) {
+        var feature = socketMap.createFeature([data.geodata.Lng, data.geodata.Lat], { id: data.id });
+        socketMap.setObservation(feature);
+    });
 };
